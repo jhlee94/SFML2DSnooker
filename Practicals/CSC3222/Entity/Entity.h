53614b733414a534b20f14e1ca86a3385b9f3a65
Jong Hoon Lee <j94117@gmail.com>
2016-02-05 00:16:40 +0000

    Vector3 Struct Implemented Entities initialised on heap Aggro Range count full functional

--- Practicals/CSC3222/Entity/Entity.h
+++ Practicals/CSC3222/Entity/Entity.h
@@ -1,19 +1,56 @@
+#include <assert.h>
+
+struct vector3 {
+	float x, y, z;
+
+	float& operator[](int index) {
+		assert(index >= 0 && index < 3);
+		return this->*members[index];
+	}
+
+	float operator[](int index) const {
+		assert(index >= 0 && index < 3);
+		return this->*members[index];
+	}
+
+	vector3 operator-(vector3 &rhs) const {
+		vector3 temp;
+		temp[0] = this->*members[0] - rhs[0];
+		temp[1] = this->*members[1] - rhs[1];
+		temp[2] = this->*members[2] - rhs[2];
+
+		return temp;
+	}
+
+	static float vector3::* const members[3];
+};
+
+float vector3::* const vector3::members[3] = { &vector3::x, &vector3::y, &vector3::z };
+
 class Entity{
 
 public:
 	Entity() {
-		v[0] = v[1] = v[2] = 0.0f;
+		v = new vector3;
 		aggroRange = 0.0f;
 		entitiesInRange = 0;
 	}
 	
 	~Entity() {
+		delete v;
+		v = NULL;
 	}
 	
 	Entity(float &x, float &y, float &z) {
-		v[0] = x;
-		v[1] = y;
-		v[2] = z;
+		v = new vector3;
+		(*v)[0] = x;
+		(*v)[1] = y;
+		(*v)[2] = z;
+	}
+
+	Entity(const Entity &obj) {
+		v = new vector3;
+		*v = *obj.v;
 	}
 
 	void setAggroRange(const float &aggro) {
@@ -24,6 +61,12 @@ public:
 		entitiesInRange = entities;
 	}
 
+	void setPosition(float &x, float &y, float &z) {
+		(*v)[0] = x;
+		(*v)[1] = y;
+		(*v)[2] = z;
+	}
+
 	float getAggroRange() const {
 		return aggroRange;
 	}
@@ -32,12 +75,12 @@ public:
 		return entitiesInRange;
 	}
 
-	float * getPosition() {
+	vector3 * getPosition() const {
 		return v;
 	}
 
 private:
-	float v[3];
+	vector3 *v;
 	float aggroRange;
 	int entitiesInRange;
 };
